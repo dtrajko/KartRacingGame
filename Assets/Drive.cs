a3cffa4b845fce43d334d512f1c1f711daa1fad3
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-12 05:08:30 +0100

    Section 2: Vehicle Setup - 8. Driving Mechanics

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -6,28 +6,51 @@ public class Drive : MonoBehaviour
 {
     public WheelCollider[] WheelColliders;
     public GameObject[] Wheels;
-    public float torque = 300.0f;
-    public float maxSteerAngle = 40.0f;
+    public float torque = 500.0f;
+    public float maxSteerAngle = 35.0f;
+    public float maxBrakeTorque = 2000.0f;
 
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    void Go(float accel, float steer)
+    void Go(float accel, float steer, float brake)
     {
         accel = Mathf.Clamp(accel, -1, 1);
         steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
-        float thrustTorque = accel * torque;
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
 
+        float thrustTorque;
+        float wheelSteer;
         Quaternion quat;
         Vector3 position;
 
         for (int i = 0; i < 4; i++)
         {
-            WheelColliders[i].motorTorque = thrustTorque;
+            switch (i)
+            {
+                // front wheels
+                case 0:
+                case 1:
+                    wheelSteer = steer;
+                    thrustTorque = 0.0f;
+                    break;
+                // back wheels
+                case 2:
+                case 3:
+                    wheelSteer = 0.0f;
+                    thrustTorque = accel * torque;
+                    break;
+                default:
+                    wheelSteer = 0.0f;
+                    thrustTorque = 0.0f;
+                    break;
+            }
 
-            float wheelSteer = i < 2 ? steer : 0.0f;
+            WheelColliders[i].motorTorque = thrustTorque;
+            WheelColliders[i].brakeTorque = brake;
+
             WheelColliders[i].steerAngle = wheelSteer;
 
             WheelColliders[i].GetWorldPose(out position, out quat);
@@ -41,6 +64,7 @@ public class Drive : MonoBehaviour
     {
         float a = Input.GetAxis("Vertical");
         float s = Input.GetAxis("Horizontal");
-        Go(a, s);
+        float b = Input.GetAxis("Jump");
+        Go(a, s, b);
     }
 }
