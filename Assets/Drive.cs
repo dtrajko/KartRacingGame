c4c4c6978b5564464f0c800da777649a1e23fea0
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-13 01:15:41 +0100

    Section 3: Vehicle Special Effects - 10. Improved Skidding

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -10,6 +10,37 @@ public class Drive : MonoBehaviour
     public float maxSteerAngle = 35.0f;
     public float maxBrakeTorque = 2000.0f;
 
+    public AudioSource skidSound;
+
+    public Transform SkidTrailPrefab;
+    Transform[] skidTrails = new Transform[4];
+
+    public void StartSkidTrail(int i)
+    {
+        if (skidTrails[i] == null)
+        {
+            skidTrails[i] = Instantiate(SkidTrailPrefab);
+        }
+
+        skidTrails[i].parent = WheelColliders[i].transform;
+        skidTrails[i].localRotation = Quaternion.Euler(90, 0, 0);
+        skidTrails[i].localPosition = -Vector3.up * WheelColliders[i].radius;
+    }
+
+    public void EndSkidTrail(int i)
+    {
+        if (skidTrails[i] == null)
+        {
+            return;
+        }
+
+        Transform holder = skidTrails[i];
+        skidTrails[i] = null;
+        holder.parent = null;
+        holder.rotation = Quaternion.Euler(90, 0, 0);
+        Destroy(holder.gameObject, 30);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,12 +90,44 @@ public class Drive : MonoBehaviour
         }
     }
 
+    void CheckForSkid()
+    {
+        int numSkidding = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            WheelHit wheelHit;
+            WheelColliders[i].GetGroundHit(out wheelHit);
+
+            if (Mathf.Abs(wheelHit.forwardSlip) >= 0.8f || Mathf.Abs(wheelHit.sidewaysSlip) >= 0.8f)
+            {
+                numSkidding++;
+                if (!skidSound.isPlaying)
+                {
+                    skidSound.Play();
+                }
+                // StartSkidTrail(i);
+            }
+            else
+            {
+                // EndSkidTrail(i);
+            }
+        }
+
+        if (numSkidding == 0 && skidSound.isPlaying)
+        {
+            skidSound.Stop();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         float a = Input.GetAxis("Vertical");
         float s = Input.GetAxis("Horizontal");
         float b = Input.GetAxis("Jump");
+
         Go(a, s, b);
+
+        CheckForSkid();
     }
 }
