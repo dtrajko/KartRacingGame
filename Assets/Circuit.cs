4b917ef63b0ec9e86788cc90a102e3688503e665
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-15 00:53:12 +0100

    Section 4: Basic NPC Vehicle Behaviour - 16. Waypoints

---
+++ Assets/Circuit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circuit : MonoBehaviour
+{
+    public GameObject[] waypoints;
+
+    private void OnDrawGizmos()
+    {
+        DrawGizmo(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        DrawGizmo(true);
+    }
+
+    void DrawGizmo(bool selected)
+    {
+        if (selected == false) return;
+        if (waypoints.Length > 1)
+        {
+            Vector3 prev = waypoints[0].transform.position;
+            for (int i = 1; i < waypoints.Length; i++)
+            {
+                Vector3 next = waypoints[i].transform.position;
+                Gizmos.DrawLine(prev, next);
+                prev = next;
+            }
+            Gizmos.DrawLine(prev, waypoints[0].transform.position);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
