e469b184ceacd0a136d1e796caee4d1380769a56
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-22 04:28:51 +0100

    Section 6: Graphical User Interface - 30. First and Third Player Switching

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera cameraObject;
+
+    float cameraSwitchSpeed = 10.0f;
+
+    enum PerspectiveModes
+    { 
+        FirstPersonPlayer = -1,
+        ThirdPersonPlayer = 1
+    }
+
+    PerspectiveModes CameraPerspective = PerspectiveModes.ThirdPersonPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("CameraPerspective"))
+        { 
+            CameraPerspective = (PerspectiveModes)PlayerPrefs.GetInt("CameraPerspective");
+        }
+
+        if (cameraObject == null)
+        {
+            cameraObject = GetComponent<Camera>();
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (CameraPerspective == PerspectiveModes.FirstPersonPlayer)
+        {
+            // First person
+            cameraObject.transform.localPosition =
+                Vector3.Lerp(cameraObject.transform.localPosition, new Vector3(-0.4f, 1.0f, 0.5f), 
+                cameraSwitchSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Third person
+            cameraObject.transform.localPosition =
+                Vector3.Lerp(cameraObject.transform.localPosition, new Vector3(0.0f, 3.0f, -9.0f),
+                cameraSwitchSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            switch (CameraPerspective)
+            {
+                case PerspectiveModes.FirstPersonPlayer:
+                    CameraPerspective = PerspectiveModes.ThirdPersonPlayer;
+                    break;
+                case PerspectiveModes.ThirdPersonPlayer:
+                    CameraPerspective = PerspectiveModes.FirstPersonPlayer;
+                    break;
+                default:
+                    CameraPerspective = PerspectiveModes.ThirdPersonPlayer;
+                    break;
+            }
+
+            PlayerPrefs.SetInt("CameraPerspective", (int)CameraPerspective);
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     Drive drive;
 
+    float lastTimeMoving = 0.0f;
+    Vector3 lastPosition;
+    Quaternion lastRotation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,18 +17,48 @@ public class PlayerController : MonoBehaviour
         this.GetComponent<Ghost>().enabled = false;
     }
 
+    void ResetLayer()
+    {
+        drive.rigidBody.gameObject.layer = LayerMask.NameToLayer("Default");
+        this.GetComponent<Ghost>().enabled = false;
+    }
+
     void Update()
     {
-        if (!RaceMonitor.racing)
+        float acceleration = Input.GetAxis("Vertical");
+        float steering     = Input.GetAxis("Horizontal");
+        float braking      = Input.GetAxis("Jump");
+
+        RaycastHit hit;
+        if (Physics.Raycast(drive.rigidBody.gameObject.transform.position, -Vector3.up, out hit, 10.0f))
         {
-            return;
+            if (hit.collider.gameObject.tag == "road")
+            {
+                lastPosition = drive.rigidBody.gameObject.transform.position;
+                lastRotation = drive.rigidBody.gameObject.transform.rotation;
+
+                if (drive.rigidBody.velocity.magnitude > 1 || !RaceMonitor.racing)
+                {
+                    lastTimeMoving = Time.time;
+                }
+            }
         }
 
-        float a = Input.GetAxis("Vertical");
-        float s = Input.GetAxis("Horizontal");
-        float b = Input.GetAxis("Jump");
+        if (Time.time > lastTimeMoving + 4.0f)
+        {
+            drive.rigidBody.gameObject.transform.position = lastPosition;
+            drive.rigidBody.gameObject.transform.rotation = lastRotation;
+            drive.rigidBody.gameObject.layer = LayerMask.NameToLayer("ReSpawn");
+            this.GetComponent<Ghost>().enabled = true;
+            Invoke("ResetLayer", 3);
+        }
 
-        drive.Go(a, s, b);
+        if (!RaceMonitor.racing)
+        {
+            acceleration = 0.0f;
+        }
+
+        drive.Go(acceleration, steering, braking);
         drive.CheckForSkid();
         drive.CalculateEngineSound();
     }
