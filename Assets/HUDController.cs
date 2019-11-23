af92bff6794a1d2d8d5b0f5299fdeb4f79c1aff7
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-23 05:24:30 +0100

    Section 6: Graphical User Interface - 33. The Heads Up Display Part 3

---
+++ Assets/HUDController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    CanvasGroup canvasGroupHUD;
+    float HUDSetting = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroupHUD = GetComponentInParent<CanvasGroup>();
+        canvasGroupHUD.alpha = 0.0f;
+
+        if (PlayerPrefs.HasKey("HUDSetting"))
+        {
+            HUDSetting = PlayerPrefs.GetFloat("HUDSetting");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!RaceMonitor.racing)
+        {
+            canvasGroupHUD.alpha = 0.0f;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            canvasGroupHUD.alpha = (canvasGroupHUD.alpha == 1.0f) ? 0.0f : 1.0f;
+            HUDSetting = canvasGroupHUD.alpha;
+            PlayerPrefs.SetFloat("HUDSetting", HUDSetting);
+        }
+    }
+}
