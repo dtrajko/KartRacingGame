9dbc1d0cc8979aa3031692b7114da94a0bd3de55
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-24 02:16:55 +0100

    Section 7: Counting Laps - 37. Detecting Checkpoints (I)

--- Assets/CheckpointManager.cs
+++ Assets/CheckpointManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CheckpointManager : MonoBehaviour
 {
+    public int lap = 0;
+    public int checkPoint = -1;
+    int checkPointCount;
+    int nextCheckPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkPointCount = GameObject.FindGameObjectsWithTag("checkpoint").Length;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "checkpoint")
+        {
+            int thisCPNumber = int.Parse(other.gameObject.name);
+            if (thisCPNumber == nextCheckPoint)
+            {
+                checkPoint = thisCPNumber;
+                if (checkPoint == 0)
+                {
+                    lap++;
+                }
+
+                nextCheckPoint++;
+                if (nextCheckPoint >= checkPointCount)
+                {
+                    nextCheckPoint = 0;
+                }
+            }
+        }
     }
 }
