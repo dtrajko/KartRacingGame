08491a91c2c0c2829d27f5dd82c125320e7d87c6
Dejan Trajkovic <dtrajko@gmail.com>
2019-11-29 14:45:58 +0100

    Section 8: Custom Play - 50. Creating a Main Menu Screen

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LaunchManager : MonoBehaviour
+{
+    public InputField playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("PlayerName"))
+        {
+            playerName.text = PlayerPrefs.GetString("PlayerName");
+        }
+    }
+
+    public void ConnectSingle()
+    {
+        SceneManager.LoadScene("Track1");
+    }
+
+    public void SetName(string name)
+    {
+        PlayerPrefs.SetString("PlayerName", name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SelectCar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCar : MonoBehaviour
+{
+    public GameObject[] cars;
+    int currentCar = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.LookAt(cars[currentCar].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentCar++;
+            if (currentCar > cars.Length - 1)
+            {
+                currentCar = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentCar--;
+            if (currentCar < 0)
+            {
+                currentCar = cars.Length - 1;
+            }
+        }
+
+        Quaternion lookDir = Quaternion.LookRotation(cars[currentCar].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, lookDir, Time.deltaTime * 4.0f);
+    }
+}
